cab343933f9ac10cad49fbd88ce220dbed7b9053
Michal <michalgrzegorczyk04@gmail.com>
2023-01-29 11:21:49 +0100

    Routing Requests

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+  const url = req.url;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body>');
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" /><button type="submit">Send</button>');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
   res.write('<body>Hello from my Node.js Server!</body>');
   res.write('</html>');
+
   res.end();
 });
 
