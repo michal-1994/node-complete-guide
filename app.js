0185a1a7497e04d48bcfb020542de8ef252bb2c8
Michal <michalgrzegorczyk04@gmail.com>
2023-02-04 13:36:29 +0100

    Serve 404 page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /** @format */
 
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -14,11 +16,11 @@ app.use(
   })
 );
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
