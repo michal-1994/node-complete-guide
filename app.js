c2b50a2f7a909d46f751f9c868733e80ce9127e1
Michal <michalgrzegorczyk04@gmail.com>
2023-02-04 12:57:37 +0100

    Parse request

--- app.js
+++ app.js
@@ -3,15 +3,27 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<h1>Add product page</h1>');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>'
+  );
 });
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>');
+app.use("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express home page</h1>");
 });
 
 app.listen(3000);
