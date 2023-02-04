17c5eae2ea1cea9b9dfd8311814b0e59e4528892
Michal <michalgrzegorczyk04@gmail.com>
2023-02-04 12:48:06 +0100

    Handle routes

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next();
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Add product page</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log("In the another middleware!");
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
