fd1321a9f736dfdfa45151dadb0e8b2f35f1db5f
Michal <michalgrzegorczyk04@gmail.com>
2023-02-11 10:19:29 +0100

    Fetch and store data

--- controllers/products.js
+++ controllers/products.js
@@ -16,10 +16,11 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    products: products,
-    pageTitle: "Shop",
-    path: "/",
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      products: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,6 +1,23 @@
 /** @format */
 
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -8,10 +25,15 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
