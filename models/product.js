b89aa349f7697deef92edd862c7ef502d506707a
Michal <michalgrzegorczyk04@gmail.com>
2023-02-12 09:37:43 +0100

    Add product id

--- models/product.js
+++ models/product.js
@@ -28,6 +28,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = new Date().getTime().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
